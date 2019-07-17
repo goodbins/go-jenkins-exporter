3c7f9b5902f6a8c9bc06d3aeab3e0ef7c874ee9b
abousselmi <bousselmi.ayoub@yahoo.fr>
2019-07-17 08:03:48 +0200

    Update many many things..

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.7.3
 	github.com/prometheus/client_golang v1.0.0
 	github.com/sirupsen/logrus v1.4.2
+	github.com/tidwall/pretty v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -11,6 +13,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
+	"github.com/tidwall/pretty"
 )
 
 // Create prometheus Gauges
@@ -53,11 +56,64 @@ var (
 	})
 )
 
+// Get data using jenkins API
+// To request data, we need the following information
+//		host: jenkins host
+//		port: jenkins port
+//		username: jenkins username
+//		password: jenkins password
+//
+var apiURL string = "http://127.0.0.1:8080/api/json/"
+var apiQuery string = "?tree=jobs[fullName,url,lastBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]],lastCompletedBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]],lastFailedBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]],lastStableBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]],lastSuccessfulBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]],lastUnstableBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]],lastUnsuccessfulBuild[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]]]"
+var jenkinsAPITimeout time.Duration = 10
+var jenkinsUsername string = "admin"
+var jenkinsPassword string = "09269d8d3892403299b61ad47795fbbe"
+var jenkinsAPIToken string = "113c32fc7f2833993ec339268f87f5a664"
+
+// Jenkins exporter host and port
+var myHost = "127.0.0.1"
+var myPort = "5000"
+
+// Types and Structs
+type jActions struct {
+	Class string `json:"_class"`
+}
+
+type jStatus struct {
+	Class     string `json:"_class"`
+	Actions   []jActions
+	Duration  int `json:"duration"`
+	Number    int `json:"number"`
+	Timestamp int `json:"timestamp"`
+}
+
+type job struct {
+	Class                 string  `json:"_class"`
+	FullName              string  `json:"fullName"`
+	URL                   string  `json:"url"`
+	LastBuild             jStatus `json:"lastBuild"`
+	LastCompletedBuild    jStatus `json:"lastCompletedBuild"`
+	LastFailedBuild       jStatus `json:"lastFailedBuild"`
+	LastStableBuild       jStatus `json:"lastStableBuild"`
+	LastSuccessfulBuild   jStatus `json:"lastSuccessfulBuild"`
+	LastUnstableBuild     jStatus `json:"lastUnstableBuild"`
+	LastUnsuccessfulBuild jStatus `json:"lastUnsuccessfulBuild"`
+}
+
+type jenkinsResponse struct {
+	Class string `json:"_class"`
+	Jobs  []job  `json:"jobs"`
+}
+
 // Init logging, prometheus collectors, etc.
 func init() {
 	//init logging
 	logrus.SetOutput(os.Stdout)
 	logrus.SetLevel(logrus.DebugLevel)
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	logrus.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
 	//register prometheus metric collectors
 	prometheus.MustRegister(numberGauge)
 	prometheus.MustRegister(durationGauge)
@@ -80,6 +136,86 @@ func init() {
 	passCountGauge.Set(0.0)
 }
 
+// Main
+func main() {
+	// Print start message
+	logrus.Debug("Starting go jenkins exporter")
+
+	// Add an API router
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", handlers.Ping).Methods("GET")
+	r.Handle("/metrics", promhttp.Handler())
+
+	// Update go routine
+	logrus.Debug("Launching metrics update loop")
+	go setGauges()
+
+	// Create the
+	go getData(apiURL, apiQuery, jenkinsAPITimeout, jenkinsUsername, jenkinsPassword)
+
+	logrus.Info("Listning on " + myHost + " port " + myPort + " ...")
+	logrus.Fatal(http.ListenAndServe(myHost+":"+myPort, r))
+}
+
+func getData(url, query string, timeout time.Duration, username, password string) map[string]interface{} {
+	// Init a map whose keys are strings and whose values are themselves
+	// stored as empty interface values
+	var jResp jenkinsResponse
+	// Init an http client
+	httpClient := &http.Client{Timeout: timeout * time.Second}
+	// Init a http request, set basic auth and Do the request
+	req, err := http.NewRequest("GET", url+query, nil)
+
+	//var bearer = "Bearer " + jenkinsAPIToken
+
+	req.SetBasicAuth(username, jenkinsAPIToken)
+	//req.Header.Set("Content-Type", "application/json")
+	//req.Header.Set("Bearer", jenkinsAPIToken)
+	resp, err := httpClient.Do(req)
+	// Panic if an error occurs
+	if err != nil {
+		logrus.Error("An error has occured when getting: %s", apiURL)
+		panic("An error has occured when trying to reach jenkins")
+	}
+	defer resp.Body.Close()
+
+	// Decode: the better version
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &jResp)
+
+	/*// Decode the response into the result map
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)*/
+	logrus.Debug("Jenkins API returned: %v", jResp)
+
+	for _, job := range jResp.Jobs {
+		logrus.Debug("Job name: %v", job.FullName)
+	}
+
+	res := pretty.Pretty(body)
+	logrus.Info(string(res))
+
+	/*
+			var jobs map[string]interface{}
+
+			for job := range result["jobs"] {
+
+			}
+		            for job in result['jobs']:
+		                if job['_class'] == 'com.cloudbees.hudson.plugins.folder.Folder' or \
+		                   job['_class'] == 'jenkins.branch.OrganizationFolder' or \
+		                   job['_class'] == 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject':
+		                    jobs += parsejobs(job['url'] + '/api/json')
+		                else:
+							jobs.append(job)
+	*/
+	// Hand back the result
+	return nil
+}
+
 // Update prometheus Gauges
 func setGauges() {
 	for {
@@ -92,35 +228,17 @@ func setGauges() {
 		failCountGauge.Set(rand.Float64())
 		totalCountGauge.Set(rand.Float64())
 		passCountGauge.Set(rand.Float64())
-		time.Sleep(1000 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 	}
 }
 
-// Get data using jenkins API
-// To request data, we need the following information
-//		host: jenkins host
-//		port: jenkins port
-//		username: jenkins username
-//		password: jenkins password
-//
-func getData() {
+/*
+func createQuery() {
+	var jobStatuses = []string{"lastBuild", "lastCompletedBuild", "lastFailedBuild", "lastStableBuild", "lastSuccessfulBuild", "lastUnstableBuild", "lastUnsuccessfulBuild"}
 
-}
-
-// Main
-func main() {
-	logrus.Debug("Starting go jenkins exporter")
-
-	r := mux.NewRouter()
-	r.HandleFunc("/ping", handlers.Ping).Methods("GET")
-	r.Handle("/metrics", promhttp.Handler())
-
-	var host = "127.0.0.1"
-	var port = "5000"
-
-	logrus.Debug("Launching metrics update loop")
-	go setGauges()
-
-	logrus.Info("Listning on " + host + " port " + port + " ...")
-	logrus.Fatal(http.ListenAndServe(host+":"+port, r))
-}
+	jobProperties = "[fullName,number,timestamp,duration,actions[queuingDurationMillis,totalDurationMillis,skipCount,failCount,totalCount,passCount]]"
+    tree = 'jobs[fullName,url,{0}]'.format(','.join([s + jobs for s in self.statuses]))
+        params = {
+            'tree': tree,
+        }
+}*/
